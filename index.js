15f386fc012fcb342508064388f30b6a076bb062
boilermaker-innovator <jgwynne7@gmail.com>
2025-01-26 11:42:14 +0800

    Update index.efv
    defvefv

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
+// Add functionality for the button
+document.getElementById("actionButton").addEventListener("click", function () {
+    document.getElementById("message").innerText = "Button Clicked! Welcome to the interactive page.";
+});
 
