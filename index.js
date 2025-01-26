19216de5d63fef915892decfc48df447437a932a
boilermaker-innovator <jgwynne7@gmail.com>
2025-01-26 11:55:52 +0800

    Update index.js
    Lrvnlingbp

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// Add functionality for the button
-document.getElementById("actionButton").addEventListener("click", function () {
-    document.getElementById("message").innerText = "Button Clicked! Welcome to the interactive page.";
+document.getElementById('actionButton').addEventListener('click', () => {
+  alert('Button clicked! Welcome to your first interactive project.');
 });
-
